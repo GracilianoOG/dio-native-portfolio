c2dd8f9ae1aa24e000e42f33d2579ba65925d06c
Gabriel <gabriel.gracilianomb@gmail.com>
2025-07-30 13:35:18 -0300

    Move styles to object and fix colors

--- src/components/SkillCard/SkillCard.tsx
+++ src/components/SkillCard/SkillCard.tsx
@@ -1,26 +1,16 @@
 import React from "react";
 import { Text, View } from "react-native";
 import { styles } from "./styles";
-import { Colors } from "react-native/Libraries/NewAppScreen";
+import { Colors } from "../../utils/colors";
 import Ionicons from "@expo/vector-icons/Ionicons";
 
 export default function SkillCard() {
   return (
-    <View
-      style={{
-        alignItems: "center",
-        borderBottomColor: Colors.ORANGE,
-        borderBottomWidth: 1,
-        flexDirection: "row",
-        justifyContent: "space-between",
-        paddingBottom: 10,
-        marginBottom: 30,
-      }}
-    >
+    <View style={styles.container}>
       <View>
-        <Text style={{ color: Colors.WHITE, fontSize: 20 }}>TypeScript</Text>
+        <Text style={styles.name}>TypeScript</Text>
       </View>
-      <View style={{ flexDirection: "row" }}>
+      <View style={styles.stars}>
         <Ionicons name="star-sharp" size={20} color={Colors.ORANGE} />
         <Ionicons name="star-sharp" size={20} color={Colors.ORANGE} />
         <Ionicons name="star-sharp" size={20} color={Colors.ORANGE} />
--- src/components/SkillCard/styles.ts
+++ src/components/SkillCard/styles.ts
@@ -1,5 +1,16 @@
 import { StyleSheet } from "react-native";
+import { Colors } from "../../utils/colors";
 
 export const styles = StyleSheet.create({
-  container: {},
+  container: {
+    alignItems: "center",
+    borderBottomColor: Colors.ORANGE,
+    borderBottomWidth: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingBottom: 10,
+    marginBottom: 30,
+  },
+  name: { color: Colors.WHITE, fontSize: 20 },
+  stars: { flexDirection: "row" },
 });
