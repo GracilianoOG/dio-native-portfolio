e36f5a93cc09dc09660779c1ab81c7aab153b953
Gabriel <gabriel.gracilianomb@gmail.com>
2025-07-30 13:12:00 -0300

    Create SkillsScreen

---
+++ src/screens/SkillsScreen/SkillsScreen.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { View } from "react-native";
+
+import { styles } from "./styles";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function SkillsScreen() {
+  return <SafeAreaView style={styles.container}></SafeAreaView>;
+}
