4ffdaa09f83e130061dd950d29a579c087099354
Gabriel <gabriel.gracilianomb@gmail.com>
2025-07-30 19:35:17 -0300

    Wrap cards with ScrollView

--- src/screens/SkillsScreen/SkillsScreen.tsx
+++ src/screens/SkillsScreen/SkillsScreen.tsx
@@ -1,13 +1,28 @@
 import React from "react";
-import { Text, View } from "react-native";
-
+import { ScrollView, Text, View } from "react-native";
 import { styles } from "./styles";
 import { SafeAreaView } from "react-native-safe-area-context";
+import SkillCard from "../../components/SkillCard";
 
 export default function SkillsScreen() {
   return (
     <SafeAreaView style={styles.container}>
       <Text style={styles.title}>Skills</Text>
+      <ScrollView>
+        <SkillCard name="HTML" rating={[1, 1, 1, 1, 1]} />
+        <SkillCard name="CSS" rating={[1, 1, 1, 1]} />
+        <SkillCard name="SASS" rating={[1, 1, 1, 0]} />
+        <SkillCard name="JavaScript" rating={[1, 1, 1, 1, 0]} />
+        <SkillCard name="TypeScript" rating={[1, 1, 1, 0]} />
+        <SkillCard name="React" rating={[1, 1, 1, 0]} />
+        <SkillCard name="React Native" rating={[1, 1, 1]} />
+        <SkillCard name="Styled Components" rating={[1, 1, 1, 0]} />
+        <SkillCard name="Accessibility" rating={[1, 1, 1, 0]} />
+        <SkillCard name="Accessibility 2" rating={[1, 1, 1, 0]} />
+        <SkillCard name="Accessibility 3" rating={[1, 1, 1, 0]} />
+        <SkillCard name="Accessibility 4" rating={[1, 1, 1, 0]} />
+        <SkillCard name="Accessibility 5" rating={[1, 1, 1, 0]} />
+      </ScrollView>
     </SafeAreaView>
   );
 }
