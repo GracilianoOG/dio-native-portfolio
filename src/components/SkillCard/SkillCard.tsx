37c8e3706c3165125c4e9deef1d1c6471214165c
Gabriel <gabriel.gracilianomb@gmail.com>
2025-07-30 14:01:19 -0300

    Generate stars based on rating array

--- src/components/SkillCard/SkillCard.tsx
+++ src/components/SkillCard/SkillCard.tsx
@@ -6,21 +6,47 @@ import Ionicons from "@expo/vector-icons/Ionicons";
 
 interface SkillCardProps {
   name: string;
+  rating: number[];
 }
 
-export default function SkillCard({ name }: SkillCardProps) {
+export default function SkillCard({ name, rating }: SkillCardProps) {
+  const generateStarts = () => {
+    const AMOUNT = 5;
+    const SIZE = 20;
+    const stars = [];
+
+    for (let i = 0; i < AMOUNT; i++) {
+      switch (rating[i]) {
+        case 1:
+          stars.push(
+            <Ionicons name="star-sharp" size={SIZE} color={Colors.ORANGE} />
+          );
+          break;
+        case 0:
+          stars.push(
+            <Ionicons
+              name="star-half-sharp"
+              size={SIZE}
+              color={Colors.ORANGE}
+            />
+          );
+          break;
+        default:
+          stars.push(
+            <Ionicons name="star-outline" size={SIZE} color={Colors.WHITE} />
+          );
+      }
+    }
+
+    return stars;
+  };
+
   return (
     <View style={styles.container}>
       <View>
         <Text style={styles.name}>{name}</Text>
       </View>
-      <View style={styles.stars}>
-        <Ionicons name="star-sharp" size={20} color={Colors.ORANGE} />
-        <Ionicons name="star-sharp" size={20} color={Colors.ORANGE} />
-        <Ionicons name="star-sharp" size={20} color={Colors.ORANGE} />
-        <Ionicons name="star-outline" size={20} color={Colors.WHITE} />
-        <Ionicons name="star-outline" size={20} color={Colors.WHITE} />
-      </View>
+      <View style={styles.stars}>{generateStarts()}</View>
     </View>
   );
 }
