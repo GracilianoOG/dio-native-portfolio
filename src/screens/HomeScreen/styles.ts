98c0d68057b65e1876107eb1b9ebb961fdae9777
Gabriel <gabriel.gracilianomb@gmail.com>
2025-07-30 11:56:18 -0300

    Add dark background color

--- src/screens/HomeScreen/styles.ts
+++ src/screens/HomeScreen/styles.ts
@@ -1,8 +1,10 @@
 import { StyleSheet } from "react-native";
+import { Colors } from "../../utils/colors";
 
 export const styles = StyleSheet.create({
   container: {
     alignItems: "center",
+    backgroundColor: Colors.VERY_DARK_GRAY,
     flex: 1,
     justifyContent: "center",
   },
