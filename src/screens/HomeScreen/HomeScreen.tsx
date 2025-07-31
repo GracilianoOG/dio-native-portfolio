83330bf08bf97a1f7cf24735c39d85e0dbe5c5a0
Gabriel <gabriel.gracilianomb@gmail.com>
2025-07-30 23:17:10 -0300

    Remove unused import from HomeScreen

--- src/screens/HomeScreen/HomeScreen.tsx
+++ src/screens/HomeScreen/HomeScreen.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Image, Text, View } from "react-native";
+import { Text, View } from "react-native";
 import { styles } from "./styles";
 import { SafeAreaView } from "react-native-safe-area-context";
 import SkillTag from "../../components/SkillTag";
