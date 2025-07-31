cd69818c984ce94b2ff6016e11753e350e827b26
Gabriel <gabriel.gracilianomb@gmail.com>
2025-07-30 22:43:19 -0300

    Add photo and basic info

--- src/screens/ContactScreen/ContactScreen.tsx
+++ src/screens/ContactScreen/ContactScreen.tsx
@@ -1,12 +1,21 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Image, Linking, Pressable, Text, View } from "react-native";
 
 import { styles } from "./styles";
 
 export default function ContactScreen() {
   return (
     <View style={styles.container}>
-      <Text>Contact</Text>
+      <Image
+        style={styles.photo}
+        source={{
+          uri: "https://avatars.githubusercontent.com/u/72778164?s=250&v=4",
+        }}
+      />
+      <Text style={styles.title}>Graciliano</Text>
+      <Text style={styles.description}>
+        Developer, Gamer, Cooking Enthusiast
+      </Text>
     </View>
   );
 }
