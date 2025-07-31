e56d754bfbacbbc3e10e5a93e9ecd3a08d54db72
Gabriel <gabriel.gracilianomb@gmail.com>
2025-07-30 22:30:08 -0300

    Create ContactScreen

---
+++ src/screens/ContactScreen/styles.ts
@@ -0,0 +1,11 @@
+import { StyleSheet } from "react-native";
+import { Colors } from "../../utils/colors";
+
+export const styles = StyleSheet.create({
+  container: {
+    alignItems: "center",
+    backgroundColor: Colors.VERY_DARK_GRAY,
+    flex: 1,
+    justifyContent: "center",
+  },
+});
