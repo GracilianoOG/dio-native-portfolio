42d31d8cc0e272be288e9d0a43e05f2e1cda84d8
Gabriel <gabriel.gracilianomb@gmail.com>
2025-07-30 22:11:45 -0300

    Add three cards

--- src/screens/EducationScreen/EducationScreen.tsx
+++ src/screens/EducationScreen/EducationScreen.tsx
@@ -3,11 +3,29 @@ import { Text, View } from "react-native";
 
 import { styles } from "./styles";
 import { SafeAreaView } from "react-native-safe-area-context";
+import EducationCard from "../../components/EducationCard";
 
 export default function EducationScreen() {
   return (
     <SafeAreaView style={styles.container}>
       <Text style={styles.title}>Education</Text>
+      <View style={{ gap: 32 }}>
+        <EducationCard
+          institution="Análise e Desenvolvimento de Sistemas"
+          location="FAETERJ Paracambi"
+          date="março 2024 - cursando"
+        />
+        <EducationCard
+          institution="Técnico em Informática"
+          location="ETE Paracambi"
+          date="fev. 2021 - dez. 2021"
+        />
+        <EducationCard
+          institution="Técnico em Informática para Internet"
+          location="IFRJ Paulo de Frontin"
+          date="fev. 2018 - dez. 2018"
+        />
+      </View>
     </SafeAreaView>
   );
 }
